cbbf831c379a4bbac7e51fbe6731bca88609f6d2
Kent VanderVelden <kent.vandervelden@gmail.com>
2022-07-27 16:04:03 -0500

    Testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ import streamlit as st
 st.title('Yo')
 st.write(np.zeros((3,3)))
 
-img = np.randint(255, size=(100, 100), dtype=np.uint8)
+img = np.random.randint(255, size=(100, 100), dtype=np.uint8)
 st.image(img, caption='Random image', output_format='PNG')
